f7ac54e1695247cc7f3d7fed1219bf08bb53dce6
Nataliia Kotenko <kotenkonata8@gmail.com>
2022-04-07 21:19:03 +0300

    feat: add html code for burger menu

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,12 @@
+//Burger menu
+
+const iconMenu = document.querySelector(".menu-icon");
+const menuBody = document.querySelector(".menu-body");
+
+if(iconMenu){
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle('lock')
+    iconMenu.classList.toggle('active');
+    menuBody.classList.toggle('active');
+  })
+}
