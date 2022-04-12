a8b700bdf14cbfdd6c2d72b4a1e69a6d2d021a4a
Nataliia Kotenko <kotenkonata8@gmail.com>
2022-04-12 19:27:07 +0300

    feat: add styles for pets page phone

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -10,3 +10,16 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+//Burger menu
+
+const iconMenu = document.querySelector(".menu-icon");
+const menuBody = document.querySelector(".menu-body");
+
+if(iconMenu){
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle('lock')
+    iconMenu.classList.toggle('active');
+    menuBody.classList.toggle('active');
+  })
+}
