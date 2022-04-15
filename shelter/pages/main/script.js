cd11dfdfb660fb964809cc136fad5927bba401c6
Nataliia Kotenko <kotenkonata8@gmail.com>
2022-04-15 20:54:51 +0300

    feat: add template from Handlebars and render for card

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,24 +1,51 @@
 //Burger menu
-(() => {
-  const refs = {
-    openMenu: document.querySelector(".menu-icon"),
-    menu: document.querySelector(".menu-body"),
-    menuItems: document.querySelectorAll(".menu-body li"),
-    body: document.querySelector('body'),
-  };
+// (() => {
+//   const refs = {
+//     openMenu: document.querySelector(".menu-icon"),
+//     menu: document.querySelector(".menu-body"),
+//     menuItems: document.querySelectorAll(".menu-body li"),
+//     body: document.querySelector('body'),
+//   };
+//
+//   function toggleModal() {
+//     refs.openMenu.classList.toggle("active");
+//     refs.menu.classList.toggle("active");
+//     refs.body.classList.toggle('lock');
+//   }
+//
+//   refs.openMenu.addEventListener("click", toggleModal);
+//
+//   refs.menu.addEventListener("click", toggleModal);
+//
+//   refs.menuItems.forEach(el => {
+//     el.addEventListener('click', toggleModal)
+//   })
+//
+// })();
 
-  function toggleModal() {
-    refs.openMenu.classList.toggle("active");
-    refs.menu.classList.toggle("active");
-    refs.body.classList.toggle('lock');
-  }
 
-  refs.openMenu.addEventListener("click", toggleModal);
+fetch("./shelter/data/pets.json")
+  .then(response => {
+    return response.json();
+  })
+  .then(renderPets);
 
-  refs.menu.addEventListener("click", toggleModal);
 
-  refs.menuItems.forEach(el => {
-    el.addEventListener('click', toggleModal)
+function renderPets(pets) {
+  const html = document.querySelector('#list-item').innerHTML.trim();
+  const template = Handlebars.compile(html);
+
+  pets.forEach(pet => {
+    const markup = template({
+      name: pet.name,
+      img: pet.img,
+    })
+
+    const whereToAdd = document.querySelector('.placeToAdd')
+
+    whereToAdd.insertAdjacentHTML('afterend', markup)
   })
 
-})();
+
+
+}
